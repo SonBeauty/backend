3f32bc5ce7cdf1e201224bfe2106457b4ca78377
sontruongng <sontng@zen-s.com>
2024-03-01 20:07:20 +0700

    [CORS] - compliment

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
-  app.enableCors({
-    origin: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: true,
   });
   await app.listen(3000);
 }
